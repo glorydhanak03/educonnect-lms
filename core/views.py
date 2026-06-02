f7140289c6e172ab486a28e2248f7d1ffce3d6b9
glorydhanak03 <glorydhanak@gmail.com>
2026-06-02 20:21:18 +0530

    create render admin

--- core/views.py
+++ core/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+def create_admin(request):
+    User = get_user_model()
+
+    if not User.objects.filter(email="mahidhanak08@gmail.com").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="mahidhanak08@gmail.com",
+            password="mahi2808"
+        )
+        return HttpResponse("Superuser created!")
+
+    return HttpResponse("User already exists!")
